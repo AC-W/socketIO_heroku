66e659f6e81ab9cc65d3f700aef15a3a9534c44d
AC-W <weiaocheng2014@gmail.com>
2022-08-13 18:49:56 -0400

    uvicorn + eventlet

--- app.py
+++ app.py
@@ -272,10 +272,10 @@ def check_move(client,data):
         print("invalid move")
 
 # Local (windows) machine debug: -->
-# import eventlet
-# import eventlet.wsgi
+import eventlet
+import eventlet.wsgi
 # import logging
 # requests_log = logging.getLogger("socketio")
 # requests_log.setLevel(logging.ERROR)
-# eventlet.wsgi.server(eventlet.listen(('', 8000)), app,log=requests_log)
+eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
 # Local (windows) machine debug: <--
