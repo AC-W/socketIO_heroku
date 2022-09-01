7b4b94e0b2fe2abd6e295d99760be910935429cc
AC-W <weiaocheng2014@gmail.com>
2022-08-31 22:21:53 -0400

    linux issue with stockfish

--- stockfish_ai.py
+++ stockfish_ai.py
@@ -1,12 +1,12 @@
-import chess
-import chess.pgn
-import chess.engine
+import chess.engine as en
 
 class StockFish_AI:
     def __init__(self):
-        self.engine = chess.engine.SimpleEngine.popen_uci("stockfish_15_x64")
-
+        self.engine = en.SimpleEngine.popen_uci("stockfish_15_x64")
+        print(self.engine)
     def make_move(self,board):
-        info = self.engine.analyse(board, chess.engine.Limit(time=0.1))
+        info = self.engine.analyse(board, en.Limit(time=0.1))
+        print(info)
         best_move = info.get('pv')[0]
+        print(best_move)
         return best_move
