35e593baad4445a9a84b2917ed69ca069e36d04f
AC-W <weiaocheng2014@gmail.com>
2022-08-31 22:27:29 -0400

    file path issue

--- stockfish_ai.py
+++ stockfish_ai.py
@@ -2,7 +2,7 @@ import chess.engine as en
 
 class StockFish_AI:
     def __init__(self):
-        self.engine = en.SimpleEngine.popen_uci("stockfish_15_x64")
+        self.engine = en.SimpleEngine.popen_uci("./stockfish_15_x64")
         print(self.engine)
     def make_move(self,board):
         info = self.engine.analyse(board, en.Limit(time=0.1))
